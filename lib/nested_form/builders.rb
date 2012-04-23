202eaa90cf616554cec32f18a32584148e979be6
Francesco Paraggio <fparaggio@gmail.com>
2012-04-24 00:29:50 +0200

    prova twitter bootstrap

--- lib/nested_form/builders.rb
+++ lib/nested_form/builders.rb
@@ -15,7 +15,7 @@ module NestedForm
 
   begin
     require 'formtastic'
-    class FormtasticBuilder < (defined?(::Formtastic::FormBuilder) ? Formtastic::FormBuilder : ::Formtastic::SemanticFormBuilder)
+    class FormtasticBuilder < (defined?(::FormtasticBootstrap::FormBuilder) ? FormtasticBootstrap::FormBuilder : ::FormtasticBootstrap::SemanticFormBuilder)
       include ::NestedForm::BuilderMixin
     end
   rescue LoadError
